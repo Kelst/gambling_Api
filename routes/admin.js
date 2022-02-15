36814c7dc29623454ebe9f4be61307551a4da8e5
VZVYN <vikt.zvynka@gmail.com>
2022-02-15 14:13:47 +0200

    ddd

--- routes/admin.js
+++ routes/admin.js
@@ -453,6 +453,20 @@ admin.put("/admin/api/trds3f2333/changeAppVisibility/",(req,res)=>{//&app_id=111
                                                         const app= await App.find({status:"pending"})
                                                         await res.json(app)
                                                     })
+                                                    admin.get("/admin/api/trds3f2333/StateApp/",async (req,res)=>{
+                                                        const app= await App.find({});
+                                                        const state={}
+                                                        state.confirmApp=app.filter(el=>el.confirm_app===true);
+                                                        state.activeApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.confirm_app===false&&el.visibility_public===true);
+                                                        state.inuseApp=app.filter(el=>el.sold===true&&el.status==="active"&&el.visibility_public===false);
+                                                        state.hideApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.visibility_public===false);
+                                                        state.banApp=app.filter(el=>el.status==="ban");
+                                                        state.pendingApp=app.filter(el=>el.status==="pending");
+
+
+                                                        await res.json(state)
+                                                    })
+
 
 
                                                     admin.put("/admin/api/trds3f2333/hideApp/",(req,res)=>{
