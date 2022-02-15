07946635e6653ab92f894b454b08d28512fcd44c
VZVYN <vikt.zvynka@gmail.com>
2022-02-15 20:05:39 +0200

    setNaming

--- routes/admin.js
+++ routes/admin.js
@@ -494,13 +494,25 @@ admin.put("/admin/api/trds3f2333/changeAppVisibility/",(req,res)=>{//&app_id=111
                                                     })
                                                     admin.get("/admin/api/trds3f2333/StateApp/",async (req,res)=>{
                                                         const app= await App.find({});
-                                                        const state={}
-                                                        state.confirmApp=app.filter(el=>el.confirm_app===true);
-                                                        state.activeApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.confirm_app===false&&el.visibility_public===true);
-                                                        state.inuseApp=app.filter(el=>el.sold===true&&el.status==="active"&&el.visibility_public===false);
-                                                        state.hideApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.visibility_public===false);
-                                                        state.banApp=app.filter(el=>el.status==="ban");
-                                                        state.pendingApp=app.filter(el=>el.status==="pending");
+                                                        const state={
+                                                            confirmApp:[],
+                                                            activeApp:[],
+                                                            inuseApp:[],
+                                                            hideApp:[],
+                                                            banApp:[],
+                                                            pendingApp:[]
+                                                        }
+                                                        try{
+                                                        state.confirmApp=app.filter(el=>el.confirm_app===true)||[];
+                                                        state.activeApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.confirm_app===false&&el.visibility_public===true)||[];
+                                                        state.inuseApp=app.filter(el=>el.sold===true&&el.status==="active"&&el.visibility_public===false)||[];
+                                                        state.hideApp=app.filter(el=>el.sold===false&&el.status==="active"&&el.visibility_public===false)||[];
+                                                        state.banApp=app.filter(el=>el.status==="ban")||[];
+                                                        state.pendingApp=app.filter(el=>el.status==="pending")||[];
+                                                        }
+                                                        catch(e){
+                                                            console.log(e);
+                                                        }
 
 
                                                         await res.json(state)
