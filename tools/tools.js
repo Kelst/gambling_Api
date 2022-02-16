b610dda9421382312dd5652b8263a8cd1df0ffbc
VZVYN <vikt.zvynka@gmail.com>
2022-02-16 22:32:48 +0200

    ddddsddd

--- tools/tools.js
+++ tools/tools.js
@@ -2,7 +2,7 @@ function getDate(){
     let res="";
     const date=new Date();
     const year=date.getFullYear();
-    const month=date.getMonth();
+    const month=date.getMonth()<10?"0"+date.getMonth()+1:date.getMonth()+1;
     const day=date.getDate();
     return `${day}:${month}:${year}`
 
